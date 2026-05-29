a39bf5d383f71943ed04b7ce70afb50169faca4a
Jenkins <jenkins@regula.by>
2026-05-29 21:05:35 +0300

    Release 9.5.18170

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["MRZBarcode"]),
     ],
     targets: [
-        .binaryTarget(name: "MRZBarcode", url: "https://pods.regulaforensics.com/MRZBarcode/9.4.17642/DocumentReaderCore_barcodemrz_9.4.17642.zip", checksum: "a4fa39e53e5d326756b5a165911fd3503dc3b12e72b97644ba930a6298eb30b3"),
+        .binaryTarget(name: "MRZBarcode", url: "https://pods.regulaforensics.com/MRZBarcode/9.5.18170/DocumentReaderCore_barcodemrz_9.5.18170.zip", checksum: "e1caddefac4b12d8247fae535396167178fac36500524347a3d4c9555f57627d"),
     ]
 )
